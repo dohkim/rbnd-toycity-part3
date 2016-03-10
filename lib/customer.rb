f6c6e89348fd7bb82033043918438e42b78a3b44
Doh <tank2705@gmail.com>
2016-03-10 11:57:24 -0800

    Finished Customers part

---
+++ lib/customer.rb
@@ -0,0 +1,32 @@
+class Customer
+	attr_reader :name
+
+	@@customers=[]
+
+	def initialize(options={})
+		@name = options[:name]
+		add_to_customers
+	end
+
+	def add_to_customers
+		if self.class.find_by_name(@name)
+			raise DuplicateCustomerError, "#{@name} already exists."
+		else
+			@@customers << self
+		end		
+	end
+
+	def self.all
+		@@customers
+	end
+
+	def self.find_by_name(name)
+		@@customers.each do |customer|			
+			if customer.name == name
+				return customer
+			end
+		end	
+		return false		
+	end
+
+end
